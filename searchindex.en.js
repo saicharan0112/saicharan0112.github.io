b8a2245039d42a7f4082ed799400717a09a47afd
Sai Lanka <sclanka@Macbook.local>
2025-10-26 22:01:39 -0500

    release only build folder

---
+++ searchindex.en.js
@@ -0,0 +1,18 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Docs",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Docs",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
